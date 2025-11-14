2d732ab57b0a4ae68f927a93aff9a1352acf29b9
vivwasik <wasik@mailbox.org>
2025-11-14 11:21:17 -0500

    add steam

--- configuration.nix
+++ configuration.nix
@@ -78,6 +78,14 @@
     sbctl
   ];
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    gamescopeSession.enable = true;
+  };
+
   services = {
     displayManager.gdm.enable = true;
     desktopManager.gnome.enable = true;
