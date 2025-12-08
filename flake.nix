0efb343bc8459ba5fb8c0ed36137ea1cc08b63b7
vivwasik <wasik@mailbox.org>
2025-12-08 13:38:28 -0500

    refactor home-manager input in flake.nix

--- flake.nix
+++ flake.nix
@@ -5,8 +5,10 @@
     nixpkgs = {
       url = "github:NixOS/nixpkgs/nixos-unstable";
     };
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     impermanence.url = "github:nix-community/impermanence";
     agenix.url = "github:ryantm/agenix";
     lanzaboote = {
