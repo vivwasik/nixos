7bb75fdac07ba8c322df766bb0e6ce4fa794cc7c
vivwasik <wasik@mailbox.org>
2026-03-04 18:21:45 -0500

    Revert "change font settings in gdm"
    This reverts commit 5fb160617e4ec0186928159687afa0736721f43b.

--- configuration.nix
+++ configuration.nix
@@ -118,11 +118,6 @@
     settings."org/gnome/mutter" = {
       experimental-features = [ "scale-monitor-framebuffer" ];
     };
-    settings."org/gnome/desktop/interface" = {
-      font-name = "FiraCode Nerd Font 11";
-      document-font-name = "FiraCode Nerd Font 12";
-      monospace-font-name = "FiraCode Nerd Font Mono 11";
-    };
   }];
 
   environment.extraInit = ''
